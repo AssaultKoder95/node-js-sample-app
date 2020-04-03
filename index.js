129fdcb5abe798f96327b14122dbfed5be2760d7
Abhishek Khanna <abhishek@imagekit.io>
2020-04-03 11:51:00 +0530

    Add env sample file & update index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const App = express();
 
 App.listen(PORT, () => console.log(`Server listening on ${PORT}`));
